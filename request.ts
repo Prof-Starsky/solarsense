48ed733b3b6c6741124f7188ba65bb8718fb7391
Jason Cheng <jasonic.c.7134@gmail.com>
2025-01-11 15:35:08 -0500

    FIX request.ts improper date

--- request.ts
+++ request.ts
@@ -6,9 +6,12 @@ async function getWeatherData(lat: string, long: string): Promise<Array<WeatherA
     const url = "https://archive-api.open-meteo.com/v1/archive";
 
     const formatDate = (date: Date): string => {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
+        const newDate = new Date(date);
+        newDate.setDate(newDate.getDate() - 2);
+        const year = newDate.getFullYear();
+        const month = String(newDate.getMonth() + 1).padStart(2, '0');
+        const day = String(newDate.getDate()).padStart(2, '0');
+    
         return `${year}-${month}-${day}`;
     };
 
